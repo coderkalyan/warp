b0c3847125b57de374c32383fd96d5214720c14d
Kalyan Sriram <kalyan@coderkalyan.com>
2025-03-04 18:22:42 -0600

    add to issue interface

--- rtl/warp_issue.v
+++ rtl/warp_issue.v
@@ -9,6 +9,47 @@ module warp_issue (
     input  wire        i_input_valid,
     input  wire [`BUNDLE_SIZE - 1:0] i_bundle0,
     input  wire [`BUNDLE_SIZE - 1:0] i_bundle1,
+    // interface to integer arithmetic pipeline 0
+    output wire [63:0] o_xarith0_op1,
+    output wire [63:0] o_xarith0_op2,
+    output wire [63:0] o_xarith0_opsel,
+    output wire [63:0] o_xarith0_sub,
+    output wire [63:0] o_xarith0_unsigned,
+    output wire [63:0] o_xarith0_cmp_mode,
+    output wire [63:0] o_xarith0_branch_equal,
+    output wire [63:0] o_xarith0_branch_invert,
+    output wire [63:0] o_xarith0_word,
+    // interface to integer arithmetic pipeline 1
+    output wire [63:0] o_xarith1_op1,
+    output wire [63:0] o_xarith1_op2,
+    output wire [63:0] o_xarith1_opsel,
+    output wire [63:0] o_xarith1_sub,
+    output wire [63:0] o_xarith1_unsigned,
+    output wire [63:0] o_xarith1_cmp_mode,
+    output wire [63:0] o_xarith1_branch_equal,
+    output wire [63:0] o_xarith1_branch_invert,
+    output wire [63:0] o_xarith1_word,
+    // interface to integer logic pipeline 0
+    output wire [63:0] o_xlogic0_op1,
+    output wire [63:0] o_xlogic0_op2,
+    output wire [63:0] o_xlogic0_opsel,
+    output wire [63:0] o_xlogic0_invert,
+    output wire [63:0] o_xlogic0_sll,
+    output wire [63:0] o_xlogic0_word,
+    // interface to integer logic pipeline 1
+    output wire [63:0] o_xlogic1_op1,
+    output wire [63:0] o_xlogic1_op2,
+    output wire [63:0] o_xlogic1_opsel,
+    output wire [63:0] o_xlogic1_invert,
+    output wire [63:0] o_xlogic1_sll,
+    output wire [63:0] o_xlogic1_word,
+    // interface to integer shift/rotate pipeline
+    output wire [63:0] o_xshift_operand,
+    output wire [63:0] o_xshift_amount,
+    output wire [63:0] o_xshift_opsel,
+    output wire [63:0] o_xshift_arithmetic,
+    output wire [63:0] o_xshift_word
 );
+endmodule
 
 `default_nettype wire
